73ef6475ee2277810be4a5438ee0198c7080374b
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 22:15:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  emoticon.library.
+  emoticon = library.keys.find
   return "Sorry, that emoticon was not found"
 end
 
