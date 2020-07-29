3f7f864358d3bd56bf03e96da88397afde239910
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 21:35:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,8 @@ def load_library(file_path)
   emoticons = YAML.load_file('./lib/emoticons.yml')
     table = {"Japanese" => {},"English" => {}}
 emoticons.each do |meaning, translations|
-    table["English"][translations[1]] = meaning
-    table["Japanese"][translations[0]] = translations[1]
+    table["English"][translations[1]] = meaning => {}
+    table["Japanese"][translations[0]] = translations[1] 
   end
   table
   #binding.pry
